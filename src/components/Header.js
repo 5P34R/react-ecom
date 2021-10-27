a17e6ea892d694070165994b61afb790476f74a2
VishDroid-dev <vk2345732@gmail.com>
2021-10-27 19:48:37 +0530

    added mbile nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,55 @@
 import React from "react";
+import { useState } from "react";
 
 const Header = () => {
-    return(
-        <nav className="mx-auto p-4">
-            <div className="flex">
-                <div>Logo</div>
-                <div className="flex ml-auto space-x-4">
-                    <h2>sign up</h2>
-                    <h2>log in</h2>
-                    <h2>contact</h2>
-                </div>
-            </div>
-        </nav>
-    );
-}
+  const mobileView = useState(false);
+  return (
+    <nav className="mx-auto p-5 shadow">
+      <div className="flex">
+        <div>E-Com</div>
 
-export default Header
+        <div className="hidden sm:flex ml-auto space-x-4">
+          <button className="bg-black p-2 rounded-full text-white mx-auto hover:text-gray-200">
+            Sign Up
+          </button>
+          <button className="bg-black p-2 rounded-full text-white mx-auto hover:text-gray-200">
+            {" "}
+            Login{" "}
+          </button>
+          <button className="bg-black p-2 rounded-full text-white mx-auto hover:text-gray-200">
+            Contact
+          </button>
+        </div>
+
+        {/* mobileView */}
+
+        <div className="ml-auto sm:hidden">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8 text-bold hover:text-2xl"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex flex-col mt-10">
+          <button className="max-w border-2 h-12">test</button>
+          <button className="max-w border-2 h-12">test</button>
+          <button className="max-w border-2 h-12">test</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
